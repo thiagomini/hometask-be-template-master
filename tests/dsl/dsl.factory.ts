0bc480fb8118ef43ff9f6bbf4468703a5522836f
Thiago Martins <thiago_valentim1@hotmail.com>
2024-03-27 13:17:27 -0300

    docs: add docs to dsl

--- tests/dsl/dsl.factory.ts
+++ tests/dsl/dsl.factory.ts
@@ -2,7 +2,12 @@ import { type Express } from 'express';
 
 import { contracts } from './contracts.dsl';
 
+/**
+ * DSL factory creates an object useful to encapsulate details of the API. If an endpoint changes the
+ * underlying DSL can be updated without changing the tests.
+ */
 export function createDSL(app: Express) {
+  // Freezing the object to prevent accidental changes
   return Object.freeze({
     contracts: contracts(app),
   });
