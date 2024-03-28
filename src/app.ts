8928ad7a88ecf02872dd63ff2d78779145722ed8
Thiago Martins <thiago_valentim1@hotmail.com>
2024-03-28 15:25:56 -0300

    refactor: extract balance controller

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import helmet from 'helmet';
 import { Op, QueryTypes } from 'sequelize';
 import { z } from 'zod';
 
+import { registerBalanceRoutes } from './controllers/balance.controller.js';
 import { registerContractRoutes } from './controllers/contracts.controller.js';
 import { registerJobsRoutes } from './controllers/jobs.controller.js';
-import { badRequest, conflict, httpError, notFound } from './errors.js';
-import { validateParamId } from './middleware/validators.js';
+import { badRequest } from './errors.js';
 import { sequelize } from './model.js';
 const app = express();
 app.use(bodyParser.json());
@@ -17,67 +17,8 @@ app.set('models', sequelize.models);
 
 registerContractRoutes(app);
 registerJobsRoutes(app);
+registerBalanceRoutes(app);
 
-app.post(
-  '/balances/deposit/:id',
-  validateParamId('Client'),
-  async (req, res) => {
-    const { Profile, Job, Contract } = req.app.get('models');
-    const { id } = req.params;
-
-    const user = await Profile.findByPk(id);
-
-    if (!user)
-      return res.status(404).json(
-        notFound({
-          detail: `Client with id ${id} not found`,
-        }),
-      );
-
-    if (user.type !== 'client') {
-      return res.status(400).json(
-        httpError({
-          status: 400,
-          title: 'Bad Request',
-          detail: 'Only clients can deposit funds',
-        }),
-      );
-    }
-
-    const totalAmountToPay = await Job.sum('price', {
-      where: {
-        paid: false,
-        '$Contract.clientId$': user.id,
-        '$Contract.status$': 'in_progress',
-      },
-      include: {
-        model: Contract,
-        as: 'Contract',
-      },
-    });
-
-    const depositAmount = req.body.amount;
-
-    if (depositAmount > totalAmountToPay * 1.25) {
-      return res.status(409).json(
-        conflict({
-          detail:
-            'Deposit amount exceeds more than 25% of the client total of jobs to pay',
-        }),
-      );
-    }
-
-    const newBalance = user.balance + depositAmount;
-
-    await user.update({
-      balance: newBalance,
-    });
-
-    return res.status(200).json({
-      newBalance,
-    });
-  },
-);
 
 app.get('/admin/best-profession', async (req, res) => {
   const schema = z
